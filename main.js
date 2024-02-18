be400fb8a376ac6af255c26359ecefd49f640ab8
Ricardo Rocha <ricardorocha9@gmail.com>
2024-02-18 10:12:42 -0300

    feat: projeto-2

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
+linhas = '';
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
-    let linha = '<tr>'
+    let linha = '<tr>';
     linha += `<td>${inputNomeAtividade.value}</td>`;
-    linha += `<td>${inputNomeAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= 7 ? `Aprovado` : `Reprovado`}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
     linha += `</tr>`;
 
-    const corpoTabela = document.querySelector(`tbody`);
-    corpoTabela.innerHTML = linha;
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
 });
